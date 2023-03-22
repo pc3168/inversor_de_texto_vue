33f98fd4e5ece63ad14d99625c927d017f530851
Paulo César <pc3168@gmail.com>
2023-03-22 14:16:06 -0300

    meu primeiro projeto vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Bem Vindo ao meu App Vue.js, primeiro Inversor de Texto"/>
+  <InversorTexto />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import InversorTexto from './components/InversorTexto.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    InversorTexto
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2664a3;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InversorTexto.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="texto" type="text" />
+    <button @click="inverter">Inverter</button>
+    <p>{{ textoInvertido }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      texto: "",
+      textoInvertido: "",
+    };
+  },
+  methods: {
+    inverter() {
+      this.textoInvertido = this.texto.split("").reverse().join("");
+    },
+  },
+};
+</script>
